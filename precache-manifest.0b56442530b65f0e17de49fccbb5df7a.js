003229fb57017dbfb4ad5f056cada29473becd25
Таня Бура <tanya.bura.99@gmail.com>
2020-04-11 23:22:30 +0300

    Updates

---
+++ precache-manifest.0b56442530b65f0e17de49fccbb5df7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4fd879b1a4bac1e0e2973afc42206d6",
+    "url": "/cursor-react3/index.html"
+  },
+  {
+    "revision": "0cb98d60b7699cb26475",
+    "url": "/cursor-react3/static/css/main.d72d19e2.chunk.css"
+  },
+  {
+    "revision": "f642394d2b15f67013e5",
+    "url": "/cursor-react3/static/js/2.0bfc91e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cursor-react3/static/js/2.0bfc91e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb98d60b7699cb26475",
+    "url": "/cursor-react3/static/js/main.4ae9dbc1.chunk.js"
+  },
+  {
+    "revision": "eb14ffe41865a82955d6",
+    "url": "/cursor-react3/static/js/runtime-main.993a00c4.js"
+  }
+]);
