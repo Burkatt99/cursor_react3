5d04989f0c1a5f44f69fa646476540fb2fbc4afd
Таня Бура <tanya.bura.99@gmail.com>
2020-04-11 23:15:38 +0300

    new commit with timer

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import "./App.css";
 import Timer from "./components/Timer";
 
 function App() {
-  const time = 10;
-  const interval = 2000;
   return (
     <div className="App">
       <Timer time="10" timeauto="20" step="2" />
